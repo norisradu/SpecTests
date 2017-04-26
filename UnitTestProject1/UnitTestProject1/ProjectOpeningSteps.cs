717c0e67055f60563ebcc9e7038d48e5a4a13c1e
norisradu <noris.gavrilas@yahoo.com>
2017-04-26 14:23:21 +0300

    Add files via upload

---
+++ UnitTestProject1/UnitTestProject1/ProjectOpeningSteps.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using TestStack.White;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.WindowItems;
+using TestStack.White.Factory;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.TabItems;
+using TechTalk.SpecFlow;
+using TestStack.White.UIItems.MenuItems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    [Binding]
+    public class ProjectOpeningSteps
+    {
+        private static Application application;
+        private static Window window;
+
+        [BeforeTestRun]
+        public static void BeforeTestRun()
+        {
+            application = Application.Launch(@"C:\Program Files (x86)\SDL\SDL Trados Studio\Studio5\SDLTradosStudio.exe");
+            Window window = application.GetWindow("Product Activation");
+            var continueButton = window.Get(SearchCriteria.ByAutomationId("_okCancelButton"));
+            try
+            {
+                continueButton.Click();
+            }
+            catch { }
+        }
+
+        [AfterTestRun]
+        public static void AfterTestRun()
+        {
+            window.Close();
+        }
+
+        [Given(@"I have successfully launched the application")]
+        public void GivenIHaveLaunchedTheApplication()
+        {
+            window = application.GetWindow(SearchCriteria.ByAutomationId("StudioWindowForm"), InitializeOption.NoCache);
+        }
+
+        [Then(@"the main window should open")]
+        public void ThenTheMainWindowShouldOpen()
+        {
+            Assert.IsNotNull(window);
+        }
+    }
+}
